34d085904ec0c1a8e24f4d9a6d950529463e5f00
caio <caiomateusgames@gmail.com>
2019-11-11 03:36:04 -0300

    Preparando para fazer o deploy no Heroku.

--- PyMovie/__init__.py
+++ PyMovie/__init__.py
@@ -27,7 +27,7 @@ from pymongo import MongoClient
 def main(global_config, **settings):
     # """ This function returns a Pyramid WSGI application.
     # """
-    # with Configurator(settings=settings) as config:
+     with Configurator(settings=settings) as config:
     #     config.include('pyramid_jinja2')
     #     config.include('.routes')
 
